a7ec20051c5e32ea74744020480193cf1070e231
gjselten <geert-jan@cg.nl>
2018-10-08 17:03:06 +0200

    overwrite the parent execute method. This does everything the same, except it checks if the response is a 401 and if so throw an error.

--- lib/service_butler/utilities/graphql_adapter.rb
+++ lib/service_butler/utilities/graphql_adapter.rb
@@ -9,6 +9,34 @@ module ServiceButler
         headers.merge!(context['headers']) if context['headers']
         headers
       end
+
+      def execute(document:, operation_name: nil, variables: {}, context: {})
+        request = Net::HTTP::Post.new(uri.request_uri)
+
+        request.basic_auth(uri.user, uri.password) if uri.user || uri.password
+
+        request["Accept"] = "application/json"
+        request["Content-Type"] = "application/json"
+        headers(context).each { |name, value| request[name] = value }
+
+        body = {}
+        body["query"] = document.to_query_string
+        body["variables"] = variables if variables.any?
+        body["operationName"] = operation_name if operation_name
+        request.body = JSON.generate(body)
+
+        response = connection.request(request)
+
+        raise StandardError.new "Connection to #{uri} failed. Response class: #{response.class}; Response code: #{response.code}" if response.code == '401'
+
+        case response
+        when Net::HTTPOK, Net::HTTPBadRequest
+          JSON.parse(response.body)
+        else
+          { "errors" => [{ "message" => "#{response.code} #{response.message}" }] }
+        end
+      end
+
     end
   end
 end
